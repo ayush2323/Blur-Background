7f91e1d8c5507825f3b1140ccc119c4440e59a82
Ayush Tiwari <62030895+ayush2323@users.noreply.github.com>
2024-04-02 18:21:11 +0530

    Update blurBackground.py

--- blurBackground.py
+++ blurBackground.py
@@ -3,19 +3,19 @@ import streamlit as st
 from PIL import Image, ImageFilter, ImageOps
 from transformers import pipeline
 
-# model = pipeline("image-segmentation", model="mattmdjaga/segformer_b2_clothes")
+model = pipeline("image-segmentation", model="mattmdjaga/segformer_b2_clothes")
 
-@st.cache_resource()
-def load_model_pipeline(task, model_path):
-    model = pipeline(task, model=model_path)
-    return model
+# @st.cache_resource()
+# def load_model_pipeline(task, model_path):
+#     model = pipeline(task, model=model_path)
+#     return model
 
-#### Image Generation ####
-st.title("Background Blur")
-model_path = ("./Models/models--mattmdjaga--segformer_b2_clothes/"
-              "snapshots/f6ac72992f938a1d0073fb5e5a06fd781f19f9a2")
+# #### Image Generation ####
+# st.title("Background Blur")
+# model_path = ("./Models/models--mattmdjaga--segformer_b2_clothes/"
+#               "snapshots/f6ac72992f938a1d0073fb5e5a06fd781f19f9a2")
 
-model = load_model_pipeline('image-segmentation', model_path)
+# model = load_model_pipeline('image-segmentation', model_path)
 
 # File uploader with the unique key from session state
 uploaded_image = st.file_uploader("Choose a photo", type=["jpg", "jpeg", "png"])
